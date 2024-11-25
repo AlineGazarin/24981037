164d536c9d258ec5b9c18b2ba28f32492a58dcf5
Aline Gazarin <alinegazarin@gmail.com>
2024-11-25 18:21:38 -0300

    Update Arduino.cpp

--- Tarefa004/Do While/Arduino.cpp
+++ Tarefa004/Do While/Arduino.cpp
@@ -1,1 +1,10 @@
+Exercícios práticos usando Arduino e conceitos de estrutura de repetição
 
+//1) "cameraData"é um vetor de inteiros com capacidade para armazenar 10 valores utilizando Arduino, como deve ser declarado?
+//2) Qual é a função do código cameraData[3] = analogRead(A1); em um programa Arduino que utiliza 
+//3) Explique como realizar a inicialização de um vetor de floats chamado "temperaturas"com os valores 25.5, 26.0, 24.8, 27.3 e 26.5.
+//4) Explique a função do código abaixo e qual seria a possível resposta do mesmo. 1 for ( int i = 0; i < 4; i ++) { 2 Serial . println ( ledPins [i ]);3 }
+//5) Com base no código da questão 4, complete a estrutura necessária para que o mesmo seja executado na IDE do Arduino.
+//6) Como utilizar a função random() para atribuir valores aleatórios a cada elemento de um vetor "randomValues"de tamanho 5?
+//7) Descreva a implementação do código a seguir: 1 for ( int i = 0; i < tamanhoVetor ; i ++) { 2 if ( sensorReadings [i ] == 150) { 3 break ; 4 }5 }
+//8) Descreva a função da instrução sizeof(sensorData) / sizeof(sensorData[0]) ao determinar o tamanho de um vetor "sensorData"em um programa Arduino.
